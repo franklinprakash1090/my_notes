e7bc20ab4191dd42748e0cc2f599e3269bfd14ae
Franklin Prakash <77559104+franklinprakash1090@users.noreply.github.com>
2025-10-05 19:09:29 +0530

    Update if_else(t,f).py

--- if_else(t,f).py
+++ if_else(t,f).py
@@ -2,7 +2,7 @@ rcb=input("Result:")
 if (rcb=="win"):
     print("Ee sala cup namade!")
 elif(rcb=="loose"):
-    print("Ee Sala Cuppu Lollipoopu !")
+    print("Ee Sala Cuppu Lollipopu !")
 
 else:
     print("invalid input")
